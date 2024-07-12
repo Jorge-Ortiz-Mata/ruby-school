1e0582950d5e71cf297029090f90a75b13285692
jorge-ortiz-sp <jorge.ortiz@sailpoint.com>
2024-07-11 19:58:16 -0600

    feat: install simplecov for test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 # spec/spec_helper.rb
 require 'factory_bot'
 require 'byebug'
+require 'simplecov'
+
+SimpleCov.start do
+  track_files '{school}/**/*.rb'
+end
+
+# Require all Ruby files in the 'lib' and 'app' directories
+# Dir['./{school}/**/*.rb'].sort.each { |file| require file }
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
