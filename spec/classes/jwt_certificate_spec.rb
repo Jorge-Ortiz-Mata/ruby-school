f2a4ea6833e24c51650fb3e7856531866cbc2cfe
jorge-ortiz-sp <jorge.ortiz@sailpoint.com>
2024-07-17 17:51:36 -0600

    uncomment specs

--- spec/classes/jwt_certificate_spec.rb
+++ spec/classes/jwt_certificate_spec.rb
@@ -14,32 +14,32 @@ RSpec.describe JwtCertificate do
       expect(token_encoded).to include 'ey'
     end
 
-    # it 'decodes the token correctly' do
-    #   key = 'someDummyKey123'
-    #   algorithm = 'HS256'
-    #   token_encoded = jwt_certificate.encode_token(key, algorithm)
-    #   payload = jwt_certificate.decode_token(token_encoded, key, algorithm)
+    it 'decodes the token correctly' do
+      key = 'someDummyKey123'
+      algorithm = 'HS256'
+      token_encoded = jwt_certificate.encode_token(key, algorithm)
+      payload = jwt_certificate.decode_token(token_encoded, key, algorithm)
 
-    #   data = payload[0]
-    #   algorithm_applied = payload[1]
+      data = payload[0]
+      algorithm_applied = payload[1]
 
-    #   expect(data['id']).to eql 1
-    #   expect(data['name']).to eql 'Jorge'
-    #   expect(data['role']).to eql 'admin'
-    #   expect(algorithm_applied['alg']).to eql algorithm
-    # end
+      expect(data['id']).to eql 1
+      expect(data['name']).to eql 'Jorge'
+      expect(data['role']).to eql 'admin'
+      expect(algorithm_applied['alg']).to eql algorithm
+    end
 
-    # it 'raise an error if the key is incorrect' do
-    #   key_one = 'someDummyKey123'
-    #   key_two = 'someDummyKey1234'
-    #   algorithm = 'HS256'
-    #   token_encoded = jwt_certificate.encode_token(key_one, algorithm)
-    #   allow(JWT).to receive(:decrypt).with(token_encoded, key_two, algorithm).and_raise(ArgumentError, 'Invalid key')
+    it 'raise an error if the key is incorrect' do
+      key_one = 'someDummyKey123'
+      key_two = 'someDummyKey1234'
+      algorithm = 'HS256'
+      token_encoded = jwt_certificate.encode_token(key_one, algorithm)
+      allow(JWT).to receive(:decrypt).with(token_encoded, key_two, algorithm).and_raise(ArgumentError, 'Invalid key')
 
-    #   result = jwt_certificate.decode_token(token_encoded, key_two, algorithm)
+      result = jwt_certificate.decode_token(token_encoded, key_two, algorithm)
 
-    #   expect(result).to be_an_instance_of(JWT::VerificationError)
-    #   expect(result.message).to eq('Signature verification failed')
-    # end
+      expect(result).to be_an_instance_of(JWT::VerificationError)
+      expect(result.message).to eq('Signature verification failed')
+    end
   end
 end
