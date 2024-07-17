40bf99b3aaf436c3c4cdcf6f97d6cf764ec6a28c
jorge-ortiz-sp <jorge.ortiz@sailpoint.com>
2024-07-17 17:15:23 -0600

    test: add specs for the jwt class

--- classes/jwt_certificate.rb
+++ classes/jwt_certificate.rb
@@ -8,33 +8,14 @@ class JwtCertificate
     @payload = payload
   end
 
-  def encode_token(key)
-    JWE.encrypt(@payload.to_json, key)
+  def encode_token(key, algorithm)
+    JWT.encode(@payload, key, algorithm)
   end
 
-  def decode_token(key, token)
-    JWE.decrypt(token, key)
-  rescue ArgumentError => e
+  def decode_token(token, key, algorithm)
+    # JWE.decrypt(token, key)
+    JWT.decode(token, key, algorithm)
+  rescue => e
     e
   end
 end
-
-
-# payload_dummy = { id: 'uid-ah38-aj289-smf8', first_name: 'Jorge', last_name: 'Ortiz', age: 25 }
-
-# secret_key = '28934u9enakjds_sdfniuu3_sufnu3nJN'
-
-# def generate_token(secret_key, payload)
-#   JWT.encode(payload, secret_key, 'HS256')
-# end
-
-# def decode_token(token, secret_key)
-#   JWT.decode(token, secret_key, 'HS256')
-# rescue JWT::VerificationError # JWT Verification Error regarding the secret_key
-#   'The secret key is invalid and can not decoded the access token properly'
-# end
-
-# encoded_token = generate_token(secret_key, payload_dummy)
-# response = decode_token(encoded_token, secret_key)
-
-# puts response
---
+++ spec/classes/jwt_certificate_spec.rb
@@ -0,0 +1,45 @@
+# spec/jwt_certificate_spec.rb
+require 'spec_helper'
+require_relative '../../classes/jwt_certificate'
+
+RSpec.describe JwtCertificate do
+  let(:jwt_certificate) { build(:jwt_certificate) }
+
+  describe 'public methods' do
+    it 'encodes the payload' do
+      key = 'someDummyKey123'
+      algorithm = 'HS256'
+      token_encoded = jwt_certificate.encode_token(key, algorithm)
+
+      expect(token_encoded).to include 'ey'
+    end
+
+    it 'decodes the token correctly' do
+      key = 'someDummyKey123'
+      algorithm = 'HS256'
+      token_encoded = jwt_certificate.encode_token(key, algorithm)
+      payload = jwt_certificate.decode_token(token_encoded, key, algorithm)
+
+      data = payload[0]
+      algorithm_applied = payload[1]
+
+      expect(data['id']).to eql 1
+      expect(data['name']).to eql 'Jorge'
+      expect(data['role']).to eql 'admin'
+      expect(algorithm_applied['alg']).to eql algorithm
+    end
+
+    it 'raise an error if the key is incorrect' do
+      key_one = 'someDummyKey123'
+      key_two = 'someDummyKey1234'
+      algorithm = 'HS256'
+      token_encoded = jwt_certificate.encode_token(key_one, algorithm)
+      allow(JWT).to receive(:decrypt).with(token_encoded, key_two, algorithm).and_raise(ArgumentError, 'Invalid key')
+
+      result = jwt_certificate.decode_token(token_encoded, key_two, algorithm)
+
+      expect(result).to be_an_instance_of(JWT::VerificationError)
+      expect(result.message).to eq('Signature verification failed')
+    end
+  end
+end
